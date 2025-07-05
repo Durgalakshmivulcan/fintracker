d30739f200503957c97f023d0c42194e71582586
Durgalakshmivulcan <durgalakshmi@vulcanflow.com>
2025-07-05 15:50:06 +0530

    removing env

--- server/server.js
+++ server/server.js
@@ -31,20 +31,12 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // MySQL connection
-require('dotenv').config();
-const mysql = require('mysql2');
-
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  ssl: {
-    rejectUnauthorized: true
-  }
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'household_tracker'
 });
-
 
 db.connect(err => {
   if (err) throw err;
